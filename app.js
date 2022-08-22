3f94216ee525f1371d3949bbfc9e97ea680db96b
SHIMIL T <shimiltharayil007@gmail.com>
2022-08-22 21:25:48 +0530

    2nd commit and some thing still to be added

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require("lodash");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let contentToAdd = [];
+let posts = [];
  const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home", {title:"HOME",content:homeStartingContent});
+  res.render("home", {title:"HOME",content:homeStartingContent,posts:posts});
 });
 
 app.get("/about",function(req,res){
@@ -24,7 +24,7 @@ app.get("/about",function(req,res){
 });
 
 app.get("/contact",function(req,res){
-  res.render("contact",{title:"Contact",content:contactContent,posts:contentToAdd});
+  res.render("contact",{title:"Contact",content:contactContent});
 });
 
 app.get("/compose",function(req,res){
@@ -38,12 +38,25 @@ let composeContent={
  newContent:req.body.addContent
 }
 console.log(composeContent);
-contentToAdd.push(composeContent);
+posts.push(composeContent);
 res.redirect("/");
 });
 
 
+app.get("/posts/:postName",function(req,res){
+  const requestedTitle =_.lowerCase(req.params.postName);
+posts.forEach(function(post){
+    const storedTitle=_.lowerCase(post.title);
+    if(requestedTitle === storedTitle){
+      
+      res.render("post",{
+        title:posts.Title,
+        content:post.newContent})
+    }else{
+    }
+})
 
+})
 
 
 
